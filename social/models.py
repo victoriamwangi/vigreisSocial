491ab6fa38d0056a2f863de05e2b8eea57cc9261
victoria mwangi <victoriamwangi021@gmail.com>
2022-06-04 13:58:57 +0300

    image upload

--- social/models.py
+++ social/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+# class User(models.Model):
+    
 class Image(models.Model):
     image_name = models.CharField(max_length=30)
+    social_user = models.ForeignKey(User, on_delete= models.CASCADE)
     image_caption = models.CharField(max_length=50)
     image = models.ImageField(upload_to = 'images/', null=True, blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -15,3 +21,6 @@ class Image(models.Model):
     
     def __str__(self):
         return self.image_name
+    
+
+    
--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,10 @@
 from django.urls import re_path 
 from . import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     re_path('^$', views.home, name= "home"),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
