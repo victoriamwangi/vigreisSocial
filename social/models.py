4a4fe6f3bd7aba02c0be979fe8d980ba4e5851f3
victoria mwangi <victoriamwangi021@gmail.com>
2022-06-04 13:32:30 +0300

    add image model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_caption = models.CharField(max_length=50)
+    image = models.ImageField(upload_to = 'images/', null=True, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.image_name
