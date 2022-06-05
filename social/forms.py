56ed0730a2b17ef4b18818cb86f786cf1ae8c086
victoria mwangi <victoriamwangi021@gmail.com>
2022-06-05 23:07:14 +0300

    add profile update form

--- social/forms.py
+++ social/forms.py
@@ -1,8 +1,13 @@
-from .models import Image
+from .models import Image, Profile
 from django import forms
 
 class NewPostForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['pub_date', 'social_user', 'user_profile']
+        
+class ProfileForm(forms.ModelForm):
+    class Meta: 
+        model = Profile
+        
         
--- social/urls.py
+++ social/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     re_path('^$', views.home, name= "home"),
     re_path('^new/post$', views.new_post, name= "new_post"),
+    re_path('^profile', views.profile, name = "profile"),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
